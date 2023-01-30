b56d2f0c63e56e850832ee36e5cfab06e86f92cc
yashwanth-2000 <yashawanth.n.ramaswamy@gmail.com>
2023-01-30 16:39:19 +0530

    API5

--- src/test/java/com/RingPayScripts/PlayStore_Scripts.java
+++ src/test/java/com/RingPayScripts/PlayStore_Scripts.java
@@ -16,6 +16,7 @@ public class PlayStore_Scripts  {
 
 //
 //	
+//	
 	private com.business.RingPay_PlayStore_Journey.RegisterUser_Mock_User play_mockuser;
 	private com.business.RingPay_PlayStore_Journey.RegisterUser_Onload play_onload;
 
